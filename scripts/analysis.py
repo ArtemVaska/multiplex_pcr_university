c76e32801054a08dabe5e772d17183744f3a7665
Anastasia Kislova <turdus.merula97@yandex.ru>
2024-05-06 12:14:49 +0300

    modified pripers parsing and sequence reverse while aligning

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -41,3 +41,58 @@ def find_primer_dimers(primers_valid: dict, window_size: int = 5) -> list:
                         break
 
     return dimers
+    
+def parse_primers(
+    gene_id: str,
+    primers_valid: dict
+) -> dict:  # находит в словаре primers_valid праймеры для одного гена
+    gene_id = gene_id + '_'
+    regex = fr"\b{gene_id}\b"
+
+    primer_one_loci = {}
+
+    for primer_id, primer_seq in primers_valid.items():
+      if re.findall(regex, primer_id):
+        primer_one_loci[primer_id] = primer_seq
+
+    return primer_one_loci
+
+
+def align(gene_seq, primer_seq) -> float:
+    aligner = Align.PairwiseAligner()
+    score = aligner.score(seq, primer)
+    return score
+
+
+def get_alignment_scores(genes: dict, primers: dict) -> dict:
+
+    alignment_scores = {}
+
+    for gene_id, gene_seq in genes.items():
+        gene_id_primers = parse_primers(gene_id, primers)
+
+        for primer_id, primer_seq in gene_id_primers.items():
+        	if re.search('R', primer_id) is not None:
+        	gene_seq = reversed(gene_seq)
+        	gene_id_alignment_score = align(gene_seq, primer_seq)
+            # if primer_id in gene_id_dimers:
+            #     is_dimer = True
+            # else:
+            #     is_dimer = False
+            alignment_scores[gene_id] = {primer_id: gene_id_alignment_score}
+
+    return alignment_scores
+
+
+def find_best_primer(alignment_scores: dict) -> str:
+
+    for gene_id, primer_id in alignment_scores.items():
+        max_primer_score_F = 0
+        max_primer_score_R = 0
+        gene_id_dimers = find_primer_dimers(alignment_scores[gene_id], window_size=5)
+
+        # сравниваем и учитываем, чтобы не было элемента в gene_id_dimers [primer_F, primer_R] или [primer_R, primer_F]
+
+
+alignment_scores = get_alignment_scores(genes, primers)
+best_primers = find_best_primer(alignment_scores)
